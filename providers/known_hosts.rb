0090c6f1f5ada581126c9959cf888b8abdf17f72
Roderik van der Veer <roderik.van.der.veer@kunstmaan.be>
2013-01-29 17:32:15 +0100

    Use the correct user and path for the remove action

--- providers/known_hosts.rb
+++ providers/known_hosts.rb
@@ -24,7 +24,8 @@ action :add do
 end
 
 action :remove do
-  known_hosts_path = node['ssh']['known_hosts_path']
+  ssh_user = new_resource.user || 'root'
+  known_hosts_path = default_or_user_path(node['ssh']['known_hosts_path'], ssh_user)
   execute "remove known_host entry for #{new_resource.host}" do
     command "ssh-keygen -R #{Shellwords.escape(new_resource.host)}"
     user ssh_user
