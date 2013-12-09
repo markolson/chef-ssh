a9830e3de3c724f112c5ec1e640b0457fd92c5da
Mark Olson <theothermarkolson@gmail.com>
2013-12-08 21:17:58 -0500

    Always include the port (defaulted to 22) in the ssh-keyscan command. Add port as a param to known_hosts LWRP.

--- providers/known_hosts.rb
+++ providers/known_hosts.rb
@@ -5,11 +5,12 @@ action :add do
   ssh_user = new_resource.user || 'root'
   known_hosts_path = default_or_user_path(node['ssh']['known_hosts_path'], ssh_user)
   host, port = new_resource.host.split(':')
+  # set the port to the default (22) if it wasn't already set
+  port = new_resource.port unless port 
 
   key = new_resource.key
   if key.nil?
-    port_arg = port.nil? ? '' : "-p #{port}"
-    results = `ssh-keyscan #{new_resource.hashed ? '-H ' : ''} #{port_arg} #{Shellwords.escape(host)}`
+    results = `ssh-keyscan #{new_resource.hashed ? '-H ' : ''} -p #{port.to_i} #{Shellwords.escape(host)}`
     Chef::Application.fatal! results.strip if key =~ /getaddrinfo/
     key = results.strip
   end
--- resources/known_hosts.rb
+++ resources/known_hosts.rb
@@ -2,6 +2,7 @@ actions :add, :remove
 default_action :add
 
 attribute :host, :kind_of => String, :name_attribute => true
+attribute :port, :kind_of => Integer, :default => 22
 attribute :hashed, :kind_of => [TrueClass, FalseClass], :default => TrueClass
 attribute :key, :kind_of => String
 attribute :user, :kind_of => String
