ac88a273d61a14acf64baa8abb67f27248ea95a6
Pierre RAMBAUD <pierre.rambaud86@gmail.com>
2014-05-23 12:51:11 +0200

    Adding matchers for known_hosts resource

--- libraries/matchers.rb
+++ libraries/matchers.rb
@@ -8,4 +8,12 @@ if defined?(ChefSpec)
   def remove_ssh_config(resource_name)
     ChefSpec::Matchers::ResourceMatcher.new(:ssh_config, :remove, resource_name)
   end
+
+  def add_ssh_known_hosts(resource_name)
+    ChefSpec::Matchers::ResourceMatcher.new(:ssh_known_hosts, :add, resource_name)
+  end
+
+  def remove_ssh_known_hosts(resource_name)
+    ChefSpec::Matchers::ResourceMatcher.new(:ssh_known_hosts, :remove, resource_name)
+  end
 end
