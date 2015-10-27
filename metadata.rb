8ca58557d5ca38b3044f5747a1071f14bb46699e
Tejay Cardon <tcardon@carekinesis.com>
2015-10-27 11:03:37 -0600

    release 0.10.10

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'tejay.cardon@gmail.com'
 license          'Apache 2.0'
 description      'LWRPs for managing SSH known_hosts and config files'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.10.9'
+version          '0.10.10'
 
 supports 'ubuntu'
 supports 'rhel'
