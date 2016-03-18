8a765dc2c8a3c0cbcb2eed0db2a67763c9956940
Ernesto Iser <eiser@advantone.com>
2016-03-18 16:50:16 -0400

    refactoring OS based package selection

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,8 +2,6 @@ default['ssh']['known_hosts_path'] = '/etc/ssh/ssh_known_hosts'
 default['ssh']['config_path'] = '/etc/ssh/ssh_config'
 
 case node['platform_family']
-when 'debian'
-  default['ssh']['packages'] = ['ssh']
 when 'rhel'
   default['ssh']['packages'] = ['openssh-server', 'openssh-clients']
 else
